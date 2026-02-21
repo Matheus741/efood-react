3a4a3d554e00ec1d5ffde1d8b20702a5b565c82c
Matheus <smatheus799vianna@gmail.com>
2026-02-21 19:17:50 -0300

    Fiz: correçao de imports no Card e ajustes de medidas da Home

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -6,28 +6,22 @@ export const Container = styled.section`
   height: 384px;                
   background-color: ${colors.secondary};
   position: relative;
-
-  
-     margin-top: -24px; 
+  display: flex; 
+  flex-direction: column;
+  align-items: center;
 `
 
 export const Logo = styled.img`
   width: 125px;                  
   height: 57.5px;               
-  position: absolute;
-  top: 40px;                     
-  left: 50%;
-  transform: translateX(-50%);
+  margin-top: 40px; 
   display: block;
 `
 
 export const Title = styled.h1`
   width: 539px;                  
   height: 84px;                 
-  position: absolute;
-  top: 236px;                    
-  left: 50%;
-  transform: translateX(-50%);
+  margin-top: 138.5px; 
   text-align: center;
 
   font-size: 36px;               
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,14 +3,15 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 1024px;
   margin: 0 auto;
-  padding: 40px 16px; 
+  padding: 80px 0; 
 `
 
 export const List = styled.ul`
   display: grid;
   grid-template-columns: repeat(2, 472px); 
   justify-content: space-between;          
-  gap: 32px;
+  column-gap: 80px;
+  row-gap: 48px;
 
   list-style: none;
   padding: 0;
